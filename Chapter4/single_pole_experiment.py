2c99e43067eafcbd7d32a3f954d1414ae2467add
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2019-05-15 18:01:45 +0300

    Fixed network graph node names for visualization

--- Chapter4/single_pole_experiment.py
+++ Chapter4/single_pole_experiment.py
@@ -79,8 +79,8 @@ def run_experiment(config_file, n_generations=100):
         print("\n\nFAILURE: Failed to find Sinle-Pole balancing controller!!!")
 
     # Visualize the experiment results
-    node_names = {-1:'X1', -2: 'X2', -3: 'X3', -4: 'X4', 0:'action'}
-    visualize.draw_net(config, best_genome, True, node_names=node_names, directory=out_dir)
+    node_names = {-1:'x', -2:'dot_x', -3:'θ', -4:'dot_θ', 0:'action'}
+    visualize.draw_net(config, best_genome, True, node_names=node_names, directory=out_dir, fmt='svg')
     visualize.plot_stats(stats, ylog=False, view=True, filename=os.path.join(out_dir, 'avg_fitness.svg'))
     visualize.plot_species(stats, view=True, filename=os.path.join(out_dir, 'speciation.svg'))
 
