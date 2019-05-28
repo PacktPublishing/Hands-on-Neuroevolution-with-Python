71a6062c5a80270335ce05c6596216ded09432ad
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2019-05-28 15:12:18 +0300

    Added code to run cart two-poles markov experiment

--- Chapter4/two_pole_markov_experiment.py
+++ Chapter4/two_pole_markov_experiment.py
@@ -157,4 +157,4 @@ if __name__ == '__main__':
     utils.clear_output(out_dir)
 
     # Run the experiment
-    run_experiment(config_path, n_generations=2)
+    run_experiment(config_path, n_generations=100)
