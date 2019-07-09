Fixed fitness calculation

Chapter6/maze_experiment.py
@@ -27,7 +27,7 @@ import novelty_archive as archive
 local_dir = os.path.dirname(__file__)
 # The directory to store outputs
 out_dir = os.path.join(local_dir, 'out')
-out_dir = os.path.join(out_dir, 'maze_objective')
+out_dir = os.path.join(out_dir, 'maze_ns')
 
 class MazeSimulationTrial:
     """
@@ -52,7 +52,7 @@ class MazeSimulationTrial:
 
 # The simulation results holder for a one trial.
 # It must be initialized before start of each trial.
-trialSim = None
+trial_sim = None
 
 def eval_individual(genome_id, genome, genomes, n_items_map, config, time_steps=400):
     """
@@ -68,11 +68,11 @@ def eval_individual(genome_id, genome, genomes, n_items_map, config, time_steps=
         The True if successful solver found.
     """
     # create NoveltyItem for genome and store it into map
-    n_item = archive.NoveltyItem(generation=trialSim.population.generation,
+    n_item = archive.NoveltyItem(generation=trial_sim.population.generation,
                                 genomeId=genome_id)
     n_items_map[genome_id] = n_item
     # run the simulation
-    maze_env = copy.deepcopy(trialSim.orig_maze_environment)
+    maze_env = copy.deepcopy(trial_sim.orig_maze_environment)
     control_net = neat.nn.FeedForwardNetwork.create(genome, config)
     goal_fitness = maze.maze_simulation_evaluate(
                                         env=maze_env, 
@@ -82,21 +82,24 @@ def eval_individual(genome_id, genome, genomes, n_items_map, config, time_steps=
 
     # Store simulation results into the agent record
     record = agent.AgenRecord(
-        generation=trialSim.population.generation,
+        generation=trial_sim.population.generation,
         agent_id=genome_id)
     record.fitness = goal_fitness
     record.x = maze_env.agent.location.x
     record.y = maze_env.agent.location.y
     record.hit_exit = maze_env.exit_found
-    record.species_id = trialSim.population.species.get_species_id(genome_id)
-    record.species_age = record.generation - trialSim.population.species.get_species(genome_id).created
+    record.species_id = trial_sim.population.species.get_species_id(genome_id)
+    record.species_age = record.generation - trial_sim.population.species.get_species(genome_id).created
     # add record to the store
-    trialSim.record_store.add_record(record)
+    trial_sim.record_store.add_record(record)
 
     # Evaluate the novelty of a genome and add the novelty item to the archive of Novelty items if appropriate
     if not maze_env.exit_found:
         # evaluate genome novelty and add it to the archive if appropriate
-        record.novelty = trialSim.archive.evaluate_individual_novelty(genome=genome, genomes=genomes, n_items_map=n_items_map)
+        record.novelty = trial_sim.archive.evaluate_individual_novelty(genome=genome, genomes=genomes, n_items_map=n_items_map)
+
+    # update fittest organisms list
+    trial_sim.archive.update_fittest_with_genome(genome=genome, n_items_map=n_items_map)
 
     return maze_env.exit_found
 
@@ -122,19 +125,26 @@ def eval_genomes(genomes, config):
             solver_id = genome_id
 
     # now adjust the archive settings and evaluate population
-    trialSim.archive.end_of_generation()
+    trial_sim.archive.end_of_generation()
     for genome_id, genome in genomes:
-        # set fitness value as novelty score of a genome in the population
-        genome.fitness = trialSim.archive.evaluate_individual_novelty(
-                                                                    genome=genome,
-                                                                    genomes=genomes,
-                                                                    n_items_map=n_items_map,
-                                                                    only_fitness=True)
+        # set fitness value as a logarithm of a novelty score of a genome in the population
+        fitness = trial_sim.archive.evaluate_individual_novelty(genome=genome,
+                                                                genomes=genomes,
+                                                                n_items_map=n_items_map,
+                                                                only_fitness=True)
+        # To avoid negative genome fitness scores we just set to zero all obtained
+        # fitness scores that is less than 1 (note we use the natural logarithm)
+        if fitness > 1:
+            fitness = math.log(fitness)
+        else:
+            fitness = 0
+        # assign the adjusted fitness score to the genome
+        genome.fitness = fitness
 
     # if successful maze solver was found then adjust its fitness 
     # to signal the finish evolution
     if solver_id is not None:
-        genomes[solver_id].fitness = math.log(config.fitness_threshold)
+        genomes[solver_id].fitness = math.log(800000) # ~=13.59
 
 
 def run_experiment(config_file, maze_env, novelty_archive, trial_out_dir, args=None, n_generations=100, silent=False):
@@ -168,8 +178,8 @@ def run_experiment(config_file, maze_env, novelty_archive, trial_out_dir, args=N
     p = neat.Population(config)
 
     # Create the trial simulation
-    global trialSim
-    trialSim = MazeSimulationTrial(maze_env=maze_env, 
+    global trial_sim
+    trial_sim = MazeSimulationTrial(maze_env=maze_env, 
                                     population=p,
                                     archive=novelty_archive)
 
@@ -196,7 +206,7 @@ def run_experiment(config_file, maze_env, novelty_archive, trial_out_dir, args=N
 
     # write the record store data
     rs_file = os.path.join(trial_out_dir, "data.pickle")
-    trialSim.record_store.dump(rs_file)
+    trial_sim.record_store.dump(rs_file)
 
     print("Record store file: %s" % rs_file)
     print("Random seed:", seed)
@@ -209,9 +219,9 @@ def run_experiment(config_file, maze_env, novelty_archive, trial_out_dir, args=N
                         0:'ANG_VEL', 1:'VEL'}
         visualize.draw_net(config, best_genome, True, node_names=node_names, directory=trial_out_dir, fmt='svg')
         if args is None:
-            visualize.draw_maze_records(maze_env, trialSim.record_store.records, view=True)
+            visualize.draw_maze_records(maze_env, trial_sim.record_store.records, view=True)
         else:
-            visualize.draw_maze_records(maze_env, trialSim.record_store.records, 
+            visualize.draw_maze_records(maze_env, trial_sim.record_store.records, 
                                         view=True, 
                                         width=args.width,
                                         height=args.height,
@@ -219,17 +229,23 @@ def run_experiment(config_file, maze_env, novelty_archive, trial_out_dir, args=N
         visualize.plot_stats(stats, ylog=False, view=True, filename=os.path.join(trial_out_dir, 'avg_fitness.svg'))
         visualize.plot_species(stats, view=True, filename=os.path.join(trial_out_dir, 'speciation.svg'))
 
+        # store NoveltyItems archive data
+        trial_sim.archive.write_fittest_to_file(path=os.path.join(trial_out_dir, 'ns_items_fittest.txt'))
+        trial_sim.archive.write_to_file(path=os.path.join(trial_out_dir, 'ns_items_all.txt'))
+
     return solution_found
 
 if __name__ == '__main__':
     # read command line parameters
-    parser = argparse.ArgumentParser(description="The maze experiment runner.")
+    parser = argparse.ArgumentParser(description="The maze experiment runner (Novelty Search).")
     parser.add_argument('-m', '--maze', default='medium', 
                         help='The maze configuration to use.')
     parser.add_argument('-g', '--generations', default=500, type=int, 
                         help='The number of generations for the evolutionary process.')
     parser.add_argument('-t', '--ns_threshold', type=float, default=6.0,
                         help="The novelty threshold value for the archive of NoveltyItems.")
+    parser.add_argument('-r', '--location_sample_rate', type=int, default=40,
+                        help="The sample rate of agent position points saving during simulation steps.")
     parser.add_argument('--width', type=int, default=400, help='The width of the records subplot')
     parser.add_argument('--height', type=int, default=400, help='The height of the records subplot')
     args = parser.parse_args()
@@ -249,12 +265,13 @@ if __name__ == '__main__':
     # Run the experiment
     maze_env_config = os.path.join(local_dir, '%s_maze.txt' % args.maze)
     maze_env = maze.read_environment(maze_env_config)
+    maze_env.location_sample_rate = args.location_sample_rate
 
     # Create novelty archive
     novelty_archive = archive.NoveltyArchive(threshold=args.ns_threshold,
                                         metric=maze.maze_novelty_metric)
 
-    print("Starting the %s maze experiment" % args.maze)
+    print("Starting the %s maze experiment (Novelty Search)" % args.maze)
     run_experiment( config_file=config_path, 
                     maze_env=maze_env, 
                     novelty_archive=novelty_archive,