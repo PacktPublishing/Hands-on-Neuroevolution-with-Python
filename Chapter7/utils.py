56b5ddf39185b8ed29c6f49233ce7701712db102
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2019-09-11 00:19:05 +0300

    Added Statistics helper class

--- Chapter7/utils.py
+++ Chapter7/utils.py
@@ -4,6 +4,25 @@
 import os
 import shutil
 
+import numpy as np
+
+class Statistics:
+    def __init__(self):
+        self.most_fit_scores = []
+        self.generation_statistics = []
+
+    def post_evaluate(self, distances, max_fitness):
+        self.generation_statistics.append(distances)
+        self.most_fit_scores.append(max_fitness)
+
+    def get_distance_mean(self):
+        avg_distance = np.array([np.array(xi).mean() for xi in self.generation_statistics])
+        return avg_distance
+
+    def get_distance_stdev(self):
+        stdev_distance = np.array([np.array(xi).std() for xi in self.generation_statistics])
+        return stdev_distance
+
 def clear_output(out_dir):
     """
     Function to clear output directory.
