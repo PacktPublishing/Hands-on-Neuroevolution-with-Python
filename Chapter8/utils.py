915f31df82c0556ec16a7d2b2700fd9cef9efbee
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2019-09-24 16:10:17 +0300

    Amended Statistics to include evaluation errors

--- Chapter8/utils.py
+++ Chapter8/utils.py
@@ -11,17 +11,17 @@ class Statistics:
         self.most_fit_scores = []
         self.generation_statistics = []
 
-    def post_evaluate(self, distances, max_fitness):
-        self.generation_statistics.append(distances)
+    def post_evaluate(self, errors, max_fitness):
+        self.generation_statistics.append(errors)
         self.most_fit_scores.append(max_fitness)
 
-    def get_distance_mean(self):
-        avg_distance = np.array([np.array(xi).mean() for xi in self.generation_statistics])
-        return avg_distance
+    def get_error_mean(self):
+        avg_error = np.array([np.array(xi).mean() for xi in self.generation_statistics])
+        return avg_error
 
-    def get_distance_stdev(self):
-        stdev_distance = np.array([np.array(xi).std() for xi in self.generation_statistics])
-        return stdev_distance
+    def get_error_stdev(self):
+        stdev_error = np.array([np.array(xi).std() for xi in self.generation_statistics])
+        return stdev_error
 
 def clear_output(out_dir):
     """
