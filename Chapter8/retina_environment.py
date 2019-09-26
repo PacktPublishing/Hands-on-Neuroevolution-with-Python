5f1b9c3831cb32a1d3802457443681b14fd36d32
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2019-09-26 12:19:07 +0300

    Implemented visual object type that can belong to both sides of the retina

--- Chapter8/retina_environment.py
+++ Chapter8/retina_environment.py
@@ -7,6 +7,7 @@ import numpy as np
 class Side(Enum):
     LEFT = 1
     RIGHT = 2
+    BOTH = 3
 
 class VisualObject:
     """
@@ -34,10 +35,10 @@ class VisualObject:
             for c, ch in enumerate(chars):
                 if ch == 'o':
                     # pixel is ON
-                    self.data[r, c] = 3.0
+                    self.data[r, c] = 1.0#3.0
                 else:
                     # pixel is OFF
-                    self.data[r, c] = -3.0
+                    self.data[r, c] = 0.0#-3.0
 
     def get_data(self):
         return self.data.flatten().tolist()
@@ -65,19 +66,20 @@ class RetinaEnvironment:
         Returns:
             the fitness score and error
         """
-        error = 0.0
+        error_sum = 0.0
         count = 0.0
         # Evaluate the detector ANN against 256 combintaions of the left and the right visual objects
         # at correct and incorrect sides of retina
         for left in self.visual_objects:
             for right in self.visual_objects:
-                error += self._evaluate(net, left, right, depth)
+                error, _ = self._evaluate(net, left, right, depth)
+                error_sum += error
                 count += 1.0
 
         
         # calculate the fitness score
-        fitness = max_fitness / (1.0 + error)
-        avg_error = error / count
+        fitness = max_fitness / (1.0 + error_sum)
+        avg_error = error_sum / count
 
         # print(avg_error, error)
         return fitness, avg_error
@@ -103,32 +105,35 @@ class RetinaEnvironment:
         right_target = 1.0 if right.side == Side.RIGHT else 0.0
         targets = [left_target, right_target]
 
-        # print(outputs[0], outputs[1])
+        outputs[0] = 1.0 if outputs[0] >= 0.5 else 0.0
+        outputs[1] = 1.0 if outputs[1] >= 0.5 else 0.0
+
+        # print(targets, outputs[0], outputs[1])
 
         # find error as a distance between outputs and groud truth
         error = (outputs[0] - targets[0]) * (outputs[0] - targets[0]) + (outputs[1] - targets[1]) * (outputs[1] - targets[1])
-        return error
+        return error, outputs
 
     def create_data_set(self):
         # set left side objects
+        self.visual_objects.append(VisualObject(". .\n. .", side=Side.BOTH))
+        self.visual_objects.append(VisualObject(". .\n. o", side=Side.BOTH))
+        self.visual_objects.append(VisualObject(". o\n. o", side=Side.LEFT))
+        self.visual_objects.append(VisualObject(". o\n. .", side=Side.BOTH))
         self.visual_objects.append(VisualObject(". o\no o", side=Side.LEFT))
-        self.visual_objects.append(VisualObject("o .\no o", side=Side.LEFT))
-        self.visual_objects.append(VisualObject(". .\no o", side=Side.LEFT))
-        self.visual_objects.append(VisualObject(". .\n. .", side=Side.LEFT))
-        self.visual_objects.append(VisualObject(". o\n. .", side=Side.LEFT))
-        self.visual_objects.append(VisualObject("o .\n. .", side=Side.LEFT))
-        self.visual_objects.append(VisualObject(". .\n. o", side=Side.LEFT))
-        self.visual_objects.append(VisualObject(". .\no .", side=Side.LEFT))
+        self.visual_objects.append(VisualObject(". .\no .", side=Side.BOTH))
+        self.visual_objects.append(VisualObject("o o\n. o", side=Side.LEFT))
+        self.visual_objects.append(VisualObject("o .\n. .", side=Side.BOTH))
 
         # set right side objects
-        self.visual_objects.append(VisualObject(". o\n. .", side=Side.RIGHT))
-        self.visual_objects.append(VisualObject("o .\n. .", side=Side.RIGHT))
-        self.visual_objects.append(VisualObject(". .\n. o", side=Side.RIGHT))
-        self.visual_objects.append(VisualObject(". .\no .", side=Side.RIGHT))
-        self.visual_objects.append(VisualObject("o o\n. o", side=Side.RIGHT))
+        self.visual_objects.append(VisualObject(". .\n. .", side=Side.BOTH))
+        self.visual_objects.append(VisualObject("o .\n. .", side=Side.BOTH))
+        self.visual_objects.append(VisualObject("o .\no .", side=Side.RIGHT))
+        self.visual_objects.append(VisualObject(". .\no .", side=Side.BOTH))
         self.visual_objects.append(VisualObject("o o\no .", side=Side.RIGHT))
-        self.visual_objects.append(VisualObject("o o\n. .", side=Side.RIGHT))
-        self.visual_objects.append(VisualObject(". .\n. .", side=Side.RIGHT))
+        self.visual_objects.append(VisualObject(". o\n. .", side=Side.BOTH))
+        self.visual_objects.append(VisualObject("o .\no o", side=Side.RIGHT))
+        self.visual_objects.append(VisualObject(". .\n. o", side=Side.BOTH))
 
     def __str__(self):
         """
